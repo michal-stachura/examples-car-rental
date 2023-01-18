0f1f426f2d55b7645d14322d45e77b9d139ca4c8
Michal Stachura <michal.stachura@santri.pl>
2023-01-18 16:12:25 +0100

    conditional - uniqueConstraint

--- car_rental/vehicles/models.py
+++ car_rental/vehicles/models.py
@@ -4,8 +4,8 @@ from django.utils.timezone import now
 
 from car_rental.members.models import Member
 
-class Car(models.Model):
 
+class Car(models.Model):
     class BRAND(Enum):
         ford = ("ford", "Ford")
         fiat = ("fiat", "Fiat")
@@ -14,19 +14,12 @@ class Car(models.Model):
         peug = ("peug", "Peugeot")
         citr = ("citr", "Citroen")
         chev = ("chev", "Chevrolet")
-        
 
-    brand = models.CharField(
-        max_length=4,
-        blank=True,
-        choices=[x.value for x in BRAND],
-        default=BRAND.ford.value[0]
-    )
+    brand = models.CharField(max_length=4, blank=True, choices=[x.value for x in BRAND], default=BRAND.ford.value[0])
     model = models.CharField(max_length=120)
     member = models.ForeignKey(Member, on_delete=models.CASCADE)
     production_year = models.PositiveSmallIntegerField(
-        choices=tuple((x,)*2 for x in range(2010, now().year +1)),
-        default=2010
+        choices=tuple((x,) * 2 for x in range(2010, now().year + 1)), default=2010
     )
     color = models.CharField(max_length=20, default="White")
     seats = models.PositiveSmallIntegerField(default=1)
@@ -35,24 +28,25 @@ class Car(models.Model):
     class Meta:
         ordering = ["brand", "production_year"]
         constraints = [
-            models.UniqueConstraint(fields=["member", "brand"], name="unique_car_brand_and_memeber")
+            models.UniqueConstraint(
+                fields=["member", "brand"],
+                name="unique_car_brand_and_memeber",
+                condition=models.Q(production_year__lte=2016),
+                violation_error_message="You can add only one old car within car brand. Old <= 2016."
+            ),
         ]
 
     def __str__(self) -> str:
         return f"{self.model} ({self.BRAND[self.brand].value[1]})"
-    
+
 
 class Motorcycle(models.Model):
 
     BRAND_SUZUKI = "suzu"
     BRAND_YAMAHA = "yama"
     BRAND_HARLEY = "harl"
-    
-    BRANDS = [
-        (BRAND_SUZUKI, "Suzuki"),
-        (BRAND_YAMAHA, "Yamaha"),
-        (BRAND_HARLEY, "Harley Davidson")
-    ]
+
+    BRANDS = [(BRAND_SUZUKI, "Suzuki"), (BRAND_YAMAHA, "Yamaha"), (BRAND_HARLEY, "Harley Davidson")]
 
     PRODUCTION_YEARS = (
         (2010, 2010),
@@ -67,20 +61,13 @@ class Motorcycle(models.Model):
         (2019, 2019),
         (2020, 2020),
         (2021, 2021),
-        (2022, 2022),        
+        (2022, 2022),
     )
 
-    brand = models.CharField(
-        max_length=4,
-        blank=True,
-        choices=BRANDS
-    )
+    brand = models.CharField(max_length=4, blank=True, choices=BRANDS)
     model = models.CharField(max_length=120)
     member = models.ForeignKey(Member, on_delete=models.CASCADE)
-    production_year = models.PositiveSmallIntegerField(
-        choices=PRODUCTION_YEARS,
-        default=PRODUCTION_YEARS[0]
-    )
+    production_year = models.PositiveSmallIntegerField(choices=PRODUCTION_YEARS, default=PRODUCTION_YEARS[0])
     color = models.CharField(max_length=20, default="White")
     seats = models.PositiveSmallIntegerField(default=1)
 
